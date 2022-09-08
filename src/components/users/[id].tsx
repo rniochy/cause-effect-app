3d105f1f9a51d86e38fe9fcdba1c80b747c4322c
rniochy <lniochy@gmail.com>
2022-09-09 00:16:46 +0100

    fetching data on panel

--- src/components/users/[id].tsx
+++ src/components/users/[id].tsx
@@ -1,9 +1,47 @@
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router-dom';
 import IUser from '../../util/user';
 import './userdata.css'
 function UserData() {
+    const [person, setPerson] = useState<IUser[]>();
+    const {id} = useParams()
+
+    useEffect (()=>{
+        //@ts-ignore
+
+        if(person === undefined){
+            fetch('../../../data/people.json').
+                then(res =>res.json()).
+                then(res=>
+                     setPerson(res.filter((elem:IUser) => String(elem.id) === String(id)))).
+                catch(err=>{});
+        }
+    }, [person])
     return (
          <div>
-                <h1>Hello</h1>
+                <ul>{person?.map(person=> person.name)} 
+                <li>
+                   <label>full name</label> 
+                </li>
+                <li>
+                <label>full name</label>
+                </li>
+                <li>
+                <label>full name</label>
+                </li>
+                <li>
+                <label>full name</label>
+                </li>
+                <li>
+                <label>full name</label>
+                </li>
+                <li>
+                <label>full name</label>
+                </li>
+                <li>
+                <label>full name</label>
+                </li>
+                </ul>
          </div>
     )
 }
--- src/util/user.ts
+++ src/util/user.ts
@@ -1,4 +1,5 @@
 export default interface IUser {
+    id: string,
     name: string,
     street: string,
     city: string,
